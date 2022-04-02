2fd140b2f61a0f5ac60a1ad6de42ab2dd997d07a
iankreisle <iankreisle@u.boisestate.edu>
2022-04-02 11:37:09 -0700

    App Base Created

---
+++ BSUNav/BSUNav/BSUNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  BSUNavApp.swift
+//  BSUNav
+//
+//  Created by Ian Kreisle on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct BSUNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BSUNav/BSUNavUITests/BSUNavUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BSUNavUITestsLaunchTests.swift
+//  BSUNavUITests
+//
+//  Created by Ian Kreisle on 4/2/22.
+//
+
+import XCTest
+
+class BSUNavUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
